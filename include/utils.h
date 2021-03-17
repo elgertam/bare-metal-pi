80e7918756891070154d51132e2a457665a2ea31
Andrew Elgert <andrew.elgert@gmail.com>
2021-03-16 23:55:54 -0400

    Print current exception level

--- include/utils.h
+++ include/utils.h
@@ -5,3 +5,4 @@
 void delay(u64 ticks);
 void put32(u64 address, u32 value);
 u32 get32(u64 address);
+u32 get_el();
--- src/kernel.c
+++ src/kernel.c
@@ -1,19 +1,32 @@
 #include "common.h"
+#include "utils.h"
 #include "mini_uart.h"
+#include "printf.h"
+
+void putc(void *p, char c) {
+    if (c == '\n') {
+        uart_send('\r');
+    }
+
+    uart_send(c);
+}
 
 void kernel_main() {
     uart_init();
-    uart_send_string("Raspberry Pi Bare Metal OS Initializing...\n");
+    init_printf(0, putc);
+    printf("\nRaspberry Pi Bare Metal OS Initializing...\n");
 
 #if RPI_VERSION == 3
-    uart_send_string("\t Board: Raspberry Pi 3\n");
+    printf("\t Board: Raspberry Pi 3\n");
 #endif
 
 #if RPI_VERSION == 4
-    uart_send_string("\t Board: Raspberry Pi 4\n");
+    printf("\tBoard: Raspberry Pi 4\n");
 #endif
 
-    uart_send_string("\n\nDone\n");
+    printf("\tException Level: %d\n", get_el());
+
+    printf("\n\nDone\n");
 
     while(1) {
         uart_send(uart_recv());
