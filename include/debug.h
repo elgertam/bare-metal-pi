09f34bfe39ea837b3ed6eb7617b5f425739d8a4b
Andrew Elgert <andrew.elgert@gmail.com>
2021-04-08 15:16:05 -0400

    Added DEBUG() macro

---
+++ include/debug.h
@@ -0,0 +1,8 @@
+#include "printf.h"
+
+#define DEBUG(format, var) printf("\tDEBUG\t"    \
+                                    "%s:%d\t"    \
+                                    "%s()\t"     \
+                                     #var ":\t"  \
+                                     format "\n" \
+                                 , __FILE__, __LINE__, __func__, var)
